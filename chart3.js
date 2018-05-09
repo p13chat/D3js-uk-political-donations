f5c0cc655cbe7e1ac2b0c24ac86165027fd4c574
Panagiotis Chatzopoulos <cena7795@hotmail.com>
2018-05-09 05:42:32 +0300

    Update chart3.js

--- chart3.js
+++ chart3.js
@@ -40,7 +40,7 @@ var comma = d3.format(",.0f");
 function transition(name) {
 	if (name === "all-data") {
 		sound.play();
-		$("#initial-content").fadeIn(700);
+		$("#initial-content").fadeIn(250);
 		$("#value-scale").fadeIn(1000);
 		$("#view-variable-type").fadeOut(250);
 		$("#view-year-type").fadeOut(250);
